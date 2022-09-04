0fb3b103b3419dbe965d7844a85ce4e9fcca53cc
Leo Souza <leonardodomaria@gmail.com>
2022-09-04 14:57:48 -0300

    create migrations

--- src/infra/repositories/typeorm/data-source.ts
+++ src/infra/repositories/typeorm/data-source.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1662313041771 } from './migrations/1662313041771-CreateRolesTable'
 
 const port = process.env.DB_PORT as number | undefined
 
@@ -11,4 +12,6 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  entities: [],
+  migrations: [CreateRolesTable1662313041771],
 })
