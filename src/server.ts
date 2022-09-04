41a205b07b047e25242cf5fccdeb5486e7cf2394
Leo Souza <leonardodomaria@gmail.com>
2022-09-04 11:53:26 -0300

    install and config swagger

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 import cors from 'cors'
 import { rolesRouter } from './routes/roles-routes'
-
+import swaggerFile from './swagger.json'
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(rolesRouter)
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
