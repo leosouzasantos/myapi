c1fa0de593897ccd629fac900c7de1c95f5663bc
Leo Souza <leonardodomaria@gmail.com>
2022-09-09 18:27:49 -0300

    user entity

--- src/infra/repositories/typeorm/data-source.ts
+++ src/infra/repositories/typeorm/data-source.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { Role } from '../../../entities/role'
+import { User } from '../../../entities/user'
 import { CreateRolesTable1662313041771 } from './migrations/1662313041771-CreateRolesTable'
 import { CreateUsersTable1662580157261 } from './migrations/1662580157261-CreateUsersTable'
 import { AddRoleIdToUsersTable1662756779476 } from './migrations/1662756779476-AddRoleIdToUsersTable'
@@ -15,7 +16,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1662313041771,
     CreateUsersTable1662580157261,
